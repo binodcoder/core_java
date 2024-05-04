b50fe3b98522a2940c9694995aa2e14be73779c0
binod <binodcoder@gmail.com>
2024-05-04 09:52:03 +0100

    going back to basic

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+
+class Student{
+    String name;
+
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+       Student s1=new Student();
+       s1.name="Binod";
+       System.out.println(s1.name);
+    }
+}
