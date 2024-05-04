ac24aa3e185d9898c311970b9a3fd54a737f8035
binod <binodcoder@gmail.com>
2024-05-04 12:38:03 +0100

    constructors in java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
+class Shape {
+    String colour;
+    double area;
+    Shape(String colour) {
+        this.colour = colour;
+    }
+    Shape(String colour, double area){
+        this.colour=colour;
+        this.area=area;
+    }
 
+    Shape(Shape s1){
+        this.colour=s1.colour;
+        this.area=s1.area;
+    }
 
-class Student{
-    String name;
+    void findArea(){
+
+    }
+
 
 }
-
 
 public class Main {
     public static void main(String[] args) {
-       Student s1=new Student();
-       s1.name="Binod";
-       System.out.println(s1.name);
+        Shape s1 = new Shape("red", 2.3);
+        Shape s2=new Shape(s1);
+        System.out.println("properties of s1");
+        System.out.println(s1.colour);
+        System.out.println(s1.area);
+        System.out.println("\n");
+        System.out.println("properties of s2");
+        System.out.println(s2.colour);
+        System.out.println(s2.area);
     }
 }
+//primitives data types
+//int, double, String, float, long , short, byte, boolean
+//wrapper classes
+//Int, Double, String, Float, Long,
