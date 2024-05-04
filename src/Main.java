6c5c8446912fb1f9b56748ad50b1f116f47078b4
Aakriti Bhusal <aakriti2061@gmail.com>
2024-05-04 14:23:59 +0100

    one constructur added

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,27 @@
 class Shape {
     String colour;
     double area;
+
     Shape(String colour) {
+
         this.colour = colour;
     }
-    Shape(String colour, double area){
-        this.colour=colour;
+
+    Shape(String colour, double area) {
+        this.colour = colour;
+        this.area = area;
+    }
+
+    Shape(double area){
         this.area=area;
     }
 
-    Shape(Shape s1){
-        this.colour=s1.colour;
-        this.area=s1.area;
+    Shape(Shape s1) {
+        this.colour = s1.colour;
+        this.area = s1.area;
     }
 
-    void findArea(){
+    void findArea() {
 
     }
 
@@ -24,7 +31,7 @@ class Shape {
 public class Main {
     public static void main(String[] args) {
         Shape s1 = new Shape("red", 2.3);
-        Shape s2=new Shape(s1);
+        Shape s2 = new Shape(s1);
         System.out.println("properties of s1");
         System.out.println(s1.colour);
         System.out.println(s1.area);
