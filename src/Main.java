07ff7f477bd5e77d6697e48584da2531718de1e8
binod <binodcoder@gmail.com>
2024-05-05 12:54:53 +0100

    Machine class added

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,22 @@ class Shape {
     Shape(String colour) {
         this.colour = colour;
     }
-    Shape(String colour, double area){
-        this.colour=colour;
-        this.area=area;
+    Shape(String colour, double area) {
+        this.colour = colour;
+        this.area = area;
     }
-
-    Shape(Shape s1){
-        this.colour=s1.colour;
-        this.area=s1.area;
+    Shape(Shape s1) {
+        this.colour = s1.colour;
+        this.area = s1.area;
     }
-
-    void findArea(){
-
+    void findArea() {
     }
-
-
 }
 
 public class Main {
     public static void main(String[] args) {
         Shape s1 = new Shape("red", 2.3);
-        Shape s2=new Shape(s1);
+        Shape s2 = new Shape(s1);
         System.out.println("properties of s1");
         System.out.println(s1.colour);
         System.out.println(s1.area);
