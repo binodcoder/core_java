8f7c3160c65537d4378f97c7ee40873d7d3cd751
binod <binodcoder@gmail.com>
2024-05-19 21:38:37 +0100

    area of  circle fixed

--- src/aakriti/AreaOfCircle.java
+++ src/aakriti/AreaOfCircle.java
@@ -1,9 +1,16 @@
 package aakriti;
-
+import java.util.Scanner;
 public class AreaOfCircle {
-    double r=5;
-    double pie=3.5;
-    double area=pie*r*r;
+    public static void main(String[] args) {
+
+        double pi=3.14;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Input radius of circle");
+        double r= scanner.nextDouble();
+        double area=pi*r*r;
+        System.out.println("The area of circle is: "+area);
+    }
+
 
 
 
