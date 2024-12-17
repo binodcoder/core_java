94e4366d44fdb9ed452c281488f1e2324de75147
binodcoder <binodcoder@gmail.com>
2024-12-17 14:42:56 +0000

    use of continue keyword..

--- src/binod/ContinueDemo.java
+++ src/binod/ContinueDemo.java
@@ -1,4 +1,6 @@
 package binod;
+import java.util.Scanner;
+
 public class ContinueDemo {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
@@ -7,5 +9,19 @@ public class ContinueDemo {
             }
             System.out.println(i);
         }
+    }
+}
+
+class ContinueDemo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Enter any number");
+            int n = sc.nextInt();
+            if (n == 10) {
+                continue;
+            }
+            System.out.println(n);
+        } while (true);
     }
 }
