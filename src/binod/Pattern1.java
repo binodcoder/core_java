3372441c2c50b6ef325a9c25f7dd710ebf7428c0
binodcoder <binodcoder@gmail.com>
2024-12-18 15:30:50 +0000

    pattern *

--- src/binod/Pattern1.java
+++ src/binod/Pattern1.java
@@ -1,8 +1,19 @@
 package binod;
 public class Pattern1 {
     public static void main(String[] args) {
-        for(int i=4;i>0;i--){
-            for(int j=0;j<i;j++){
+        for (int i = 4; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Pattern2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
