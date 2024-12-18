ae88bbdedd870261bd7ac73ca6a4094d76b9c10a
binodcoder <binodcoder@gmail.com>
2024-12-18 22:02:09 +0000

    functions

--- src/binod/AreaSquare.java
+++ src/binod/AreaSquare.java
@@ -1,11 +1,22 @@
 package binod;
 import java.util.Scanner;
+
 public class AreaSquare {
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+
+    public static double calculateSquare(double side) {
+        double area = side * side;
+        return area;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter side of a square");
-        double side=scanner.nextDouble();
-        double area=side*side;
-        System.out.println("The area of square is: "+area);
+        double side = scanner.nextDouble();
+        printHelloWorld();
+        double area = calculateSquare(side);
+        System.out.println("The area of square is: " + area);
     }
 }
