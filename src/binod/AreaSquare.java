bdb247074f81dc07a44915c6ae2504c05e46ac05
binodcoder <binodcoder@gmail.com>
2024-12-19 14:44:29 +0000

    java always call by value

--- src/binod/AreaSquare.java
+++ src/binod/AreaSquare.java
@@ -11,6 +11,29 @@ public class AreaSquare {
         return area;
     }
 
+    public static void swap(int a, int b) {
+        //swap
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+    }
+
+    public static int findProduct(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+
+    public static int findFactorial(int n){
+        int fact=1;
+
+        for(int i=1;i<=n;i++){
+            fact*=i;
+        }
+        return fact;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter side of a square");
@@ -18,5 +41,8 @@ public class AreaSquare {
         printHelloWorld();
         double area = calculateSquare(side);
         System.out.println("The area of square is: " + area);
+        swap(1, 2);
+        System.out.println(findProduct(2, 3));
+        System.out.println(findFactorial(3));
     }
 }
